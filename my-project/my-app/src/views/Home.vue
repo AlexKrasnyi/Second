3667fdd6c89dd49884711f62652e3bb9ae0226db
AlexKrasnyi <AlexKrasnyi@users.noreply.github.com>
2020-04-12 19:36:12 +0300

    12.04.20 pm

---
+++ my-project/my-app/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MyApp />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MyApp from '@/components/MyApp.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    MyApp
+  }
+}
+</script>
---
+++ my-project/my-app/src/views/SignIn.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+    <form>
+        <div class="form-group">
+            <label for="email">Email address</label>
+            <input type="email" class="form-control" id="email">
+            
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+    
+        <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
