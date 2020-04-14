32135b5bc07395f554015a31f265a4e32d344535
AlexKrasnyi <AlexKrasnyi@users.noreply.github.com>
2020-04-14 08:57:43 +0300

    14.04.20 1

--- vue-yout/vue-yout/src/views/Login.vue
+++ vue-yout/vue-yout/src/views/Login.vue
@@ -67,6 +67,7 @@
 
 <script>
 import { email, required, minLength } from 'vuelidate/lib/validators'
+import messages from '@/utils/messages'
 export default {
 	name: 'login',
 	data: () => ({
@@ -77,11 +78,15 @@ export default {
 		email: { email, required },
 		password: { required, minLength: minLength(7) }
 	},
+	mounted () {
+		if (messages[this.$route.query.message]) {
+			this.$message(messages[this.$route.query.message])
+		}
+	},
 	methods: {
 		onSubmit () {
 			if (this.$v.$invalid) {
 				this.$v.$touch()
-				return
 			}
 
 			const formData = {
